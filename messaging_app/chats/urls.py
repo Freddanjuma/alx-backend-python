a9ac4eb1ba4aba32cd4b0f38d6eb843f15d72391
Fred Danjuma <freddanjuma247@gmail.com>
2025-11-14 19:01:52 -0500

    feat(views): implement ConversationViewSet and MessageViewSet

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
+from django.contrib import admin
 
 router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+
+    # Include chats app API routes under /api/
+    path('api/', include('chats.urls')),
 ]
