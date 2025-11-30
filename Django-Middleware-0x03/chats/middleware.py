b1a24d1a8c334ed2b34719013d5a888a46bef2fe
Fred Danjuma <freddanjuma247@gmail.com>
2025-11-30 09:12:00 -0500

    Implement RequestLoggingMiddleware and fix project structure

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,25 +1,16 @@
-import logging
 from datetime import datetime
 
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
-        """Middleware initialization"""
         self.get_response = get_response
 
-        # Configure logger
-        logging.basicConfig(
-            filename="requests.log",
-            level=logging.INFO,
-            format="%(message)s"
-        )
-
     def __call__(self, request):
-        """This runs for every request"""
-
         user = request.user if request.user.is_authenticated else "Anonymous"
-        log_entry = f"{datetime.now()} - User: {user} - Path: {request.path}"
 
-        logging.info(log_entry)
+        log_line = f"{datetime.now()} - User: {user} - Path: {request.path}\n"
+
+        with open("requests.log", "a") as f:
+            f.write(log_line)
 
         response = self.get_response(request)
         return response
