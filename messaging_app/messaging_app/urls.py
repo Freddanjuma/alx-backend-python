a9ba7cc5acb8b76dc3008b0b5c6f1096f575cf27
Fred Danjuma <freddanjuma247@gmail.com>
2026-01-20 18:48:51 -0500

    feat: containerize application and add project configuration

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -1,18 +1,62 @@
+"""
+URL configuration for messaging_app project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+"""
+# messaging_app/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+
+router = DefaultRouter()
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # API endpoints from the chats app
-    path('api/', include('chats.urls')),
-    # Browsable API login
-    path('api-auth/', include('rest_framework.urls')),
-    
-    # JWT Authentication endpoints
+    # JWT token endpointsfor users to POST credentials in order to obtain an access and refresh token.
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('api/', include('chats.urls')),  # the app route
+    path('api/', include('messaging.urls')),  # the app route
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include(router.urls)),
+]
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+router = DefaultRouter()
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # JWT token endpoints
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # App URLs
+    path('', include('chats.urls')),
+    path('', include('messaging.urls')),
+
+    path('', include(router.urls)),
 ]
