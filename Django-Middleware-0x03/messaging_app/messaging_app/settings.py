8c1178826ad8d5c87dea34d2430c7d989fa6049e
Fred Danjuma <freddanjuma247@gmail.com>
2026-01-27 22:19:17 -0500

    Move ingress.yaml and commands.txt to messaging_app folder

--- Django-Middleware-0x03/messaging_app/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/messaging_app/settings.py
@@ -9,9 +9,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-safe-for-dev-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "0.0.0.0",
+]
 
 
 # Application definition
